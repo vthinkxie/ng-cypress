053fd9a4a981562761559db040dd3dde68e5c724
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-11-29 11:20:38 +0800

    fix: fix ts config

--- cypress/integration/visual-regression.spec.ts
+++ cypress/integration/visual-regression.spec.ts
@@ -1,6 +1,6 @@
 context('Visual Regression', () => {
   beforeEach(() => {
-    cy.viewport(1440, 900);
+    cy.viewport(1440, 900, {log: true});
     cy.visit('http://localhost:4200');
   });
   it('should screenshot', () => {
